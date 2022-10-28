cbbe413016933724db3b7cde0bc6bd77e857a5fd
J_Mundondo <jephiastmundondo@gmail.com>
2022-10-28 09:53:51 +0200

    error on onlyOnwener

--- contracts/RoundToken.sol
+++ contracts/RoundToken.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 
+
 contract RoundToken is ERC20 , ERC20Detailed {
     constructor() ERC20Detailed("Round", "RND", 18) public {
         _mint(msg.sender, 1000000);
--- contracts/RoundTokenCrowedSale.sol
+++ contracts/RoundTokenCrowedSale.sol
@@ -5,5 +5,37 @@ import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 
 contract RoundTokenCrowedSale is Crowdsale {
 
+// Crowdsale Stages
+  enum CrowdsaleStage { PreICO, ICO }
+// Default to presale stage
+  CrowdsaleStage public stage = CrowdsaleStage.PreICO;
+
+    constructor (uint256 rate, address payable wallet, IERC20 token) 
+     Crowdsale(rate, wallet, token) public 
+     {
+
+     }
+
+     /**
+  * @dev Allows admin to update the crowdsale stage
+  * @param _stage Crowdsale stage
+  */
+  function setCrowdsaleStage(uint _stage) public onlyOwner {
+    if(uint(CrowdsaleStage.PreICO) == _stage) {
+      stage = CrowdsaleStage.PreICO;
+    } else if (uint(CrowdsaleStage.ICO) == _stage) {
+      stage = CrowdsaleStage.ICO;
+    }
+
+    if(stage == CrowdsaleStage.PreICO) {
+        // rate at pre ico
+      rate = 500;
+    } else if (stage == CrowdsaleStage.ICO) {
+        // rate at crowdsalestage
+      rate = 250;
+    }
+  }
+
+
     
 }
