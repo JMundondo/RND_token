e336aa75339b8813258d465358e0c91ece95d059
J_Mundondo <jephiastmundondo@gmail.com>
2022-10-31 06:35:37 +0200

    _rate is now public

--- contracts/RoundTokenCrowedSale.sol
+++ contracts/RoundTokenCrowedSale.sol
@@ -2,25 +2,31 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract RoundTokenCrowedSale is Crowdsale {
+contract RoundTokenCrowedSale is Crowdsale ,Ownable  {
+
+ 
 
 // Crowdsale Stages
   enum CrowdsaleStage { PreICO, ICO }
 // Default to presale stage
   CrowdsaleStage public stage = CrowdsaleStage.PreICO;
 
+
+ 
+
     constructor (uint256 rate, address payable wallet, IERC20 token) 
      Crowdsale(rate, wallet, token) public 
      {
-
+       
      }
 
      /**
   * @dev Allows admin to update the crowdsale stage
   * @param _stage Crowdsale stage
   */
-  function setCrowdsaleStage(uint _stage) public onlyOwner {
+  function setCrowdsaleStage(uint _stage ,uint _setRate ) public onlyOwner  {
     if(uint(CrowdsaleStage.PreICO) == _stage) {
       stage = CrowdsaleStage.PreICO;
     } else if (uint(CrowdsaleStage.ICO) == _stage) {
@@ -29,10 +35,10 @@ contract RoundTokenCrowedSale is Crowdsale {
 
     if(stage == CrowdsaleStage.PreICO) {
         // rate at pre ico
-      rate = 500;
+      _rate = _setRate;
     } else if (stage == CrowdsaleStage.ICO) {
         // rate at crowdsalestage
-      rate = 250;
+      _rate = _setRate;
     }
   }
 
