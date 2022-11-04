514add48c1a34ae2aa922dec1d4c9f35279b9b90
TinasheMundondo <jephiastmundondo@gmail.com>
2022-11-04 16:12:47 +0200

    deployeble

--- migrations/2_RoundToken_migration.js
+++ migrations/2_RoundToken_migration.js
@@ -1,7 +1,16 @@
 const RoundToken = artifacts.require("./RoundToken.sol");
 const RoundTokenCrowdsale = artifacts.require("./RoundTokenCrowdsale.sol");
 
-const ether = (n) => new web3.BigNumber(web3.toWei(n, 'ether'));
+const duration = {
+  seconds: function (val) { return val; },
+  minutes: function (val) { return val * this.seconds(60); },
+  hours: function (val) { return val * this.minutes(60); },
+  days: function (val) { return val * this.hours(24); },
+  weeks: function (val) { return val * this.days(7); },
+  years: function (val) { return val * this.days(365); },
+};
+
+/*const ether = (n) => new web3.BigNumber(web3.toWei(n, 'ether'));
 
 const duration = {
   seconds: function (val) { return val; },
@@ -12,7 +21,7 @@ const duration = {
   years: function (val) { return val * this.days(365); },
 };
 
-module.exports = async function(deployer, network, accounts) {
+module.exports = async function(deployer, accounts) {
 
 
   await deployer.deploy(RoundToken);
@@ -40,4 +49,29 @@ module.exports = async function(deployer, network, accounts) {
   );
 
   return true;
+};*/
+module.exports =  async function (deployer ,accounts) {
+  await deployer.deploy(RoundToken);
+  const deployedToken = await RoundToken.deployed();
+  const latestTime = (new Date).getTime();
+
+const _rate           = 500;
+const _wallet         = '0x98428f27787200b8bC20E62A4a5670f97D177d03'; // TODO: Replace me
+const _token          = deployedToken.address;
+//const _openingTime    = latestTime + duration.minutes(1);
+const _openingTime    = latestTime ;
+const _closingTime    = _openingTime + duration.weeks(1);
+const _cap            = 1190401570;
+
+await deployer.deploy(
+  RoundTokenCrowdsale,
+  _rate,
+  _wallet,
+  _token,
+  _cap,
+  _openingTime,
+  _closingTime,
+ 
+);
 };
+
--- truffle-config.js
+++ truffle-config.js
@@ -89,17 +89,7 @@ module.exports = {
       port: 8545,
       network_id: '*', // eslint-disable-line camelcase
     },
-   /* ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`
-        )
-      },
-      gas: 5000000,
-      gasPrice: 25000000000,
-      network_id: 3
-    }*/
+  
   },
 
   // Set default mocha options here, use special reporters etc.
