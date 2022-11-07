cc6214f253debb2262907afe4f15d0e8ccd98d06
J_Mundondo <jephiastmundondo@gmail.com>
2022-11-07 12:57:50 +0200

    roundToken is now capped ,burnable, mintable

--- contracts/RoundToken.sol
+++ contracts/RoundToken.sol
@@ -3,11 +3,18 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 
 
 
-contract RoundToken is ERC20 , ERC20Detailed {
-    constructor() ERC20Detailed("Round", "RND", 18) public {
+contract RoundToken is ERC20 , ERC20Detailed ,ERC20Mintable,ERC20Capped {
+    constructor() 
+    ERC20Detailed("Round", "RND", 18) 
+    ERC20Capped(50000000)
+  
+    public {
         _mint(msg.sender, 1000000);
     }
     
