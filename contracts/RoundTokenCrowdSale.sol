6403b2e84b37da0409b551c2082651e6c599c4e7
J_Mundondo <jephiastmundondo@gmail.com>
2022-11-14 11:05:22 +0200

    latest repo

--- contracts/RoundTokenCrowdSale.sol
+++ contracts/RoundTokenCrowdSale.sol
@@ -8,53 +8,19 @@ import "@openzeppelin/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
 contract RoundTokenCrowdSale is Crowdsale ,Ownable  {
 
- 
-
-// Crowdsale Stages
-  enum CrowdsaleStage { PreICO, ICO }
-// Default to presale stage
-  CrowdsaleStage public stage = CrowdsaleStage.PreICO;
-
-
- 
-
-     constructor(
+  constructor(
         uint256 rate,            // rate, in TKNbits
         address payable wallet,  // wallet to send Ether
         IERC20 token           // the token
-                           // closing time in unix epoch seconds
-    )
-       
-     
-     
-        Crowdsale(rate, wallet, token)
-        public
+        )
+Crowdsale(rate, wallet, token) public
     {
-        // nice, we just created a crowdsale that's only open
-        // for a certain amount of time
-        // and stops accepting contributions once it reaches `cap`
+       
     }
 
-     /**
-  * @dev Allows admin to update the crowdsale stage
-  * @param _stage Crowdsale stage
-  */
-  function setCrowdsaleStage(uint _stage ,uint _setRate ) public onlyOwner  {
-    if(uint(CrowdsaleStage.PreICO) == _stage) {
-      stage = CrowdsaleStage.PreICO;
-    } else if (uint(CrowdsaleStage.ICO) == _stage) {
-      stage = CrowdsaleStage.ICO;
-    }
 
-    if(stage == CrowdsaleStage.PreICO) {
-        // rate at pre ico
-      _rate = _setRate;
-    } else if (stage == CrowdsaleStage.ICO) {
-        // rate at crowdsalestage
-      _rate = _setRate;
-    }
-  }
 
+   
 
     
 }
--- truffle-config.js
+++ truffle-config.js
@@ -41,7 +41,7 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
-  contracts_build_directory: './client/src/artifacts/' ,
+  contracts_build_directory: './RND_token_Front_end/src/artifacts/' ,
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
